48050caa541230dad1f29ead12a63245713c561d
Gopal N D <gopalhelpyou@gmail.com>
2024-06-04 16:58:56 +0530

    completed pattrens

--- pattrens/patt2.cpp
+++ pattrens/patt2.cpp
@@ -1,6 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+void pat20(int n){
+    int space = 2*n-2;
+    for(int i=1;i<=2*n-1;i++){
+        //star
+        int star = i;
+        if(i>n) star = 2*n-i;
+        for(int j=1;j<=star;j++){
+            cout << "*";
+        }
+        for(int j =1;j<=space;j++){
+            cout << " ";
+        }
+        for(int j=1;j<=star;j++){
+            cout << "*";
+        }
+        cout << endl;
+        if(i<n) space -=2;
+        else space +=2;
+    }
+}
+
+
+void pat22(int n){
+  for (int i=0;i<2*n-1;i++){
+    for(int j=0;j<2*n-1;j++){
+        int top =i;
+        int bottom = j;
+        int right = (2*n-2)-j;
+        int left = (2*n-2)-i;
+        cout << (n -min(min(top,bottom),min(left,right)));
+    }
+    cout << endl;
+  }
+}
+
 void pat19(int n){
     int space =0;
     int Rspace =8;
@@ -10,7 +45,7 @@ for(int i=0;i<n;i++){
         cout << "*";
     }
     //space 
-    for (int j =0;j<space;j++){
+    for (int j =0;j<=space;j++){
         cout << " ";
 
     }
@@ -27,7 +62,7 @@ for(int i=1;i<=n;i++){
         cout << "*";
     }
     //Rspace 
-    for (int j =0;j<Rspace;j++){
+    for (int j =1;j<Rspace;j++){
         cout << " ";
 
     }
@@ -39,8 +74,14 @@ for(int i=1;i<=n;i++){
     cout <<endl;
 }
 }
+
+
+
+
 int  main(){
     //functions
-    pat19(4);
+    // pat19(4);
+    // pat20(5);
+    pat22(5);
     return 0;
 }
