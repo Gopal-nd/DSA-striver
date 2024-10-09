cab1b466329d7837f298671aedd7eb4446564f5e
Gopal <gopalhelpyou@gmail.com>
2024-10-09 17:30:19 +0530

    resetup

---
+++ Readme.md
@@ -0,0 +1,57 @@
+## Dsa in C++
+
+### Basic to know before starting
+
+#### how set up the workspace and makesure everything is uptodatae
+
+` g++ --version ` to check the version and sussessfull installation of the compiler
+
+`cd workingFolder ` before u run the program
+
+`g++ filename.cpp` to compile the program
+
+it will provide **a.exe** file to execute 
+
+`./a.exe` to run the program
+
+---
+### simple input and output example
+
+```cpp
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a;
+    cin >>a;
+    cout <<"a value is "<< a << endl;
+    return 0;
+}
+```
+---
+
+### external resourse of programming
+
+##### email: **docode999**
+
+[TakeYouForward](https://takeuforward.org/strivers-a2z-dsa-course/strivers-a2z-dsa-course-sheet-2/)
+
+[Leetcode](https://leetcode.com/u/QZWebfpcYO/)
+
+[Github](https://github.com/gopal-nd)
+
+---
+### key thing to remember
+
+#### notes for the conceptual concepts
+1. dry run the problems in detail
+2. solve hard problems with dry run
+3. write key notes 
+
+#### Problem solving 
+1. approuch 
+2. corner cases
+3. upload code
+
+
+#### Indexing the solutions with page numbers
---
+++ past-learning/learn-basics-1/basics/one.cpp
@@ -0,0 +1,187 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+
+
+
+// void Printname(string name){
+//     cout <<name;
+// }
+
+// int sum(int n,int m){
+//     return n+m;
+// }
+
+// int maxx(int n, int m){
+//     if(n>m) {
+//         return n;
+//     }else{
+//         return m;
+//     }
+// }
+
+// void dodomething(int num[]){
+// num[0]=78;
+// }
+// int main(){
+
+//     string num = "Gopal";
+   
+//     cout << num<<endl;
+//     int n=5;
+//     int arr [n];
+//     for (int i =0;i<=4;i++){
+//         cin >> arr[i] ;
+//     }
+//      dodomething(arr);
+//      for (int i =0;i<=4;i++){
+//         cout << arr[i] ;
+//     }
+//     // int a;
+//     // int b;
+
+//     // int n = maxx( 4,4);
+//     // cout << n;
+//     // int ans = sum(2,4);
+//     // cout << ans;
+//     //--------------------//
+// //basic in put and out put
+//    // cin -input ..cin  >> varname
+//    //cout  -output ..cn  << varname ...endl to end line
+// // basic data types
+//   // int float ,double , lang short ,long long
+//   //string char , getline(cn,varname)
+// //------------------------------------------------//
+// //
+// // string str,str2;
+// // cin >> str;
+// // cin >> str2;
+// // cout << str <<"     "<<str2<<endl;
+// // string me;
+// // getline(cin,me);
+// // cout << me;
+// // int age ;
+// // cin>> age;
+// // if(age>=18){
+// //     cout << "you are an adult" <<endl;
+// // }else if(age>10){
+// //     cout << "Your are an a teenager"<<endl;
+// // }
+// // else{
+// //     cout << "You are not an adult"<< endl;
+// // }
+
+
+// //  a school as a following rules for grading system
+// ///*below 25 -f ,25to 44 -e ,45-49-d,50-59-c,60-79-b,80-100-a
+
+// //*/
+
+// // int marks;
+// // cin >> marks;
+// // if(marks<25){
+// //     cout<<"You a achived F ";
+// // }else if( marks<=44){
+// //     cout <<"Your Grade is E";
+// // }else if(marks<=47){
+// //     cout <<"Your Grade is D";
+// // }else if(marks<=59){
+// //     cout <<"Your Grade is C";
+// // }else if(marks<=69){
+// //     cout << "Your Gerade is B";
+// // }else if(marks<=79){
+// //     cout <<"Your Grade is B";
+// // }else if(marks<=100){
+// //     cout<< "Your Grade is A";
+// // }
+
+// // int day;
+// // cin >> day;
+// // if(age<18){
+// //     cout << "youa are not an adult";
+
+// // } else if(age<=57){
+// //     cout << "Elegebal for job";
+// //     if(age>=54){
+// //     cout << "You will Retire soon";
+// // }
+// // }else{
+// //     cout <<"retirement time";
+// // }
+
+// // switch(day){
+// //     case 1:
+// //     cout << "Monday";
+// //     break;
+// //     case 2:
+// //     cout << "Tuseday";
+// //     break;
+// //     case 3:
+// //     cout << "Wednesday";
+// //     break;
+// //     case 4:
+// //     cout << "Thursday";
+// //     break;
+// //     case 5:
+// //     cout << "friday";
+// //     break;
+// //     case 6:
+// //     cout << "sateday";
+// //     break;
+// //     case 7:
+// //     cout << "sunday";
+// //     break;
+// //     default:
+// //     cout << "not valid" << day<<endl;
+// // }
+
+// // int arr[5];
+// // cin >> arr[0]>>arr[1]>>arr[2]>>arr[3]>>arr[4];
+// // cout <<arr[1]+10;
+// // cout <<arr[4];
+
+// // int arr[2][2];
+// // cin >> arr[1][1];
+// // cout << arr[1][1]<<endl;
+// // cout << arr[0][0];
+
+// // string s="Gopal Reddy";
+// // cout << s << endl;
+
+// // int len = s.length();
+// // cout << s[len-1] << endl;
+// // cout << s[len]<< endl;
+
+// // for loops
+
+// // int i=3;
+// // while (i<5)
+// // {
+// //     /* code */
+// //     i++;
+// // }
+
+// // do{
+// // cout << "gopal"<<endl;
+// // i++;
+// // }while(i<6);
+// // Printname("Gopal");
+// return 0;
+// }
+
+// // functions
+// // void return parameter non parametre
+
+
+#include<iostream>
+using namespace std:
+
+int main(){
+//  int a = [1,2,3,4,5];
+ // revese an array
+ int a ,b;
+ cin>>a>>b;
+ cout << "a is "<<a<<"b is "<<b;
+return 0;
+}
+
